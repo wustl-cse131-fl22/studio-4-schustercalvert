982e859dd653fc6a478edc03b5e9e6cc6522f751
stsloaner <stsloaner@172.27.49.136>
2022-10-13 12:13:54 -0500

    done

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,104 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+
+		if (shape.equals("ellipse"))
+		{
+			if (in.nextBoolean())
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double semiMajorAxis = in.nextDouble();
+				double semiMinorAxis = in.nextDouble();
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			else
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double semiMajorAxis = in.nextDouble();
+				double semiMinorAxis = in.nextDouble();
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			
+		}
+		else if (shape.equals("rectangle"))
+		{
+			if (in.nextBoolean())
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfwidth = in.nextDouble();
+				double halfhight = in.nextDouble();
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.filledRectangle(x, y, halfwidth, halfhight);
+			}
+			else
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfwidth = in.nextDouble();
+				double halfhight = in.nextDouble();
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.rectangle(x, y, halfwidth, halfhight);
+			}
+		}
+		else if (shape.equals("triangle"))
+		{
+			if (in.nextBoolean())
+			{
+				double x1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double y3 = in.nextDouble();
+				double[] x = new double[3];
+				double[] y = new double[3];
+				x[0] = x1;
+				x[1] = x2;
+				x[2] = x3;
+				
+				y[0] = y1;
+				y[1] = y2;
+				y[2] = y3;
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.filledPolygon(x,y);
+			}
+			else
+			{
+				double x1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double y3 = in.nextDouble();
+				double[] x = new double[3];
+				double[] y = new double[3];
+				x[0] = x1;
+				x[1] = x2;
+				x[2] = x3;
+				
+				y[0] = y1;
+				y[1] = y2;
+				y[2] = y3;
+				StdDraw.setPenRadius(0.05);
+				StdDraw.setPenColor(r, g, b);
+				StdDraw.polygon(x, y);
+			}
+			
+		}
 		
 	}
 }
